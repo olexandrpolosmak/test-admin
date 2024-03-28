4066521f0b7a50dce8f6647eb9c0001d2e6519d1
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2024-03-28 21:49:27 +0200

    Add init command

---
+++ app/Console/Commands/InitCommand.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Description of INIT.php
+ * @copyright Copyright (c) DOTSPLATFORM, LLC
+ * @author    Oleksandr Polosmak <o.polosmak@dotsplatform.com>
+ */
+
+namespace App\Console\Commands;
+
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class InitCommand extends Command
+{
+    protected $signature = 'init:app
+    {weakPassword: Provide a weak password for the user, for example: 123456}';
+
+    public function handle(): void
+    {
+        $this->createUser();
+        $this->makeStoragePublic();
+    }
+
+    private function createUser(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admmin@gmail.com',
+            'status' => User::STATUS_ACTIVE,
+            'level' => User::LEVEL_USER,
+            'phone' => '380998494',
+            'password' => Hash::make($this->argument('weakPassword')),
+        ]);
+    }
+
+    private function makeStoragePublic(): void
+    {
+
+        $storage = __DIR__ . "/../../../storage";
+        system("sudo chmod -R 777 " . $storage);
+    }
+
+
+}
